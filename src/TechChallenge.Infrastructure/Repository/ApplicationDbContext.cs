13fb46faa8f65c372e803d02505bccb3246a2a67
Bárbara Queiroz <barbaraqs@gmail.com>
2026-08-09 20:52:28 -0300

    Configurando dbset

--- src/TechChallenge.Infrastructure/Repository/ApplicationDbContext.cs
+++ src/TechChallenge.Infrastructure/Repository/ApplicationDbContext.cs
@@ -10,4 +10,28 @@ public class ApplicationDbContext : DbContext
     {
         _connectionString = connectionString;
     }
+
+    /* TO DOs
+     * 1. Add DbSet properties for your entities here
+     * Example:
+     * public DbSet<YourEntity> YourEntities { get; set; }
+     */
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /* TO DOs
+         * 1. Configure your entity mappings here
+         * Criar classes de configuração, pasta configuration, para cada entidade e aplicar aqui
+         * Example:
+         * modelBuilder.ApplyConfigurationFromAssembly(typeOf(ApplicationDbContext).Assembly);
+         */
+    }
 }
