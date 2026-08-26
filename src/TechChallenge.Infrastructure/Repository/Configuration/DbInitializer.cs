33798e2c6258cabdec74e76094bdddc149ca218b
Priscila Pereira <priscila.pereira755@gmail.com>
2026-08-26 20:40:10 -0300

    Criando usuários fake

--- src/TechChallenge.Infrastructure/Repository/Configuration/DbInitializer.cs
+++ src/TechChallenge.Infrastructure/Repository/Configuration/DbInitializer.cs
@@ -7,20 +7,60 @@ public static class DbInitializer
 {
     public static async Task InitializeAsync(ApplicationDbContext context)
     {
-        var adminExists = await context.Users.AnyAsync(x => x.Login == "admin");
+        var users = new List<User>
+        {
+            new User(
+                "Administrator",
+                "admin@fiap.com",
+                "admin",
+                "Admin@123",
+                PerfilEnum.Admin
+            ),
 
-        if (adminExists)
-            return;
+            new User(
+                "Barbara",
+                "barbara@fiap.com",
+                "Babi",
+                "User@456",
+                PerfilEnum.Client
+            ),
 
-        var admin = new User(
-            "Administrator",
-            "admin@fiap.com",
-            "admin",
-            "Admin@123",
-            PerfilEnum.Admin
-        );
+            new User(
+                "Micael",
+                "micael2@fiap.com",
+                "Micaelzin",
+                "User@789",
+                PerfilEnum.Client
+            ),
 
-        context.Users.Add(admin);
+
+            new User(
+                "Priscila",
+                "Priscila@fiap.com",
+                "Pris",
+                "User@012",
+                PerfilEnum.Client
+            ),
+
+            new User(
+                "Gustavo",
+                "gustavo@fiap.com",
+                "Gusta",
+                "User@123",
+                PerfilEnum.Client
+            )
+        };
+
+        foreach (var user in users)
+        {
+            var userExists = await context.Users
+                .AnyAsync(x => x.Login == user.Login);
+
+            if (!userExists)
+            {
+                context.Users.Add(user);
+            }
+        }
 
         await context.SaveChangesAsync();
     }
